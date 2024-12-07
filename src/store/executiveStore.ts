d7fde986135f6a35387e3a32fbf118a499b4924c
demilade111 <64733187+demilade111@users.noreply.github.com>
2024-12-06 17:24:18 -0800

    feat(store): Add executive state management with persistence - Implemented fetchExecutives to load executive data on first visit - Added validateExecutives to check for backend updates - Enabled persistence using zustand middleware

---
+++ src/store/executiveStore.ts
@@ -0,0 +1,63 @@
+import { createJSONStorage, persist } from "zustand/middleware";
+
+import { Executive } from "@/types/member";
+import axios from "axios";
+import { create } from "zustand";
+
+interface ExecutiveStoreState {
+  executives: Executive[];
+  isLoaded: boolean;
+  fetchExecutives: () => Promise<void>;
+  validateExecutives: () => Promise<void>;
+}
+
+export const useExecutiveStore = create(
+  persist<ExecutiveStoreState>(
+    (set, get) => ({
+      executives: [],
+      isLoaded: false,
+
+      // Fetch executives from API
+      fetchExecutives: async () => {
+        if (get().isLoaded) return; 
+        try {
+          const response = await axios.get<{ executives: Executive[] }>(
+            "https://api3.langaracs.ca/executives/all"
+          );
+          // const response = await axios.get<{ executives: Executive[] }>(
+          //   `${process.env.REACT_BASE_URL}/executives/all`
+          // );
+          if (response.data.executives) {
+            set({ executives: response.data.executives, isLoaded: true });
+          }
+        } catch (error) {
+          console.error("Error fetching executives:", error);
+        }
+      },
+
+      // Validate data by checking for updates in the backend
+      validateExecutives: async () => {
+        try {
+          const response = await axios.get<{ executives: Executive[] }>(
+            "https://api3.langaracs.ca/executives/all"
+          );
+          const fetchedExecutives = response.data.executives;
+          const currentExecutives = get().executives;
+
+          if (
+            Array.isArray(fetchedExecutives) &&
+            fetchedExecutives.length !== currentExecutives.length
+          ) {
+            set({ executives: fetchedExecutives });
+          }
+        } catch (error) {
+          console.error("Error validating executives:", error);
+        }
+      },
+    }),
+    {
+      name: "executive-storage",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
